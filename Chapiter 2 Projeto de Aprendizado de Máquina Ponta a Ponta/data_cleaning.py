105ec29e31c0e33145e4407e81657be219348046
Bruno Maciel Machado <brunomacielm@gmail.com>
2021-10-30 19:56:28 -0300

    Creates the first implementation according to the book.

---
+++ Chapiter 2 Projeto de Aprendizado de Máquina Ponta a Ponta/data_cleaning.py
@@ -0,0 +1,32 @@
+
+import pandas as pd
+
+class data_cleaner_and_manipulator():
+    def __init__(self):
+        pass
+
+    def treat_NaN(self, df, type, cols = None):
+        '''
+        Most machine learning algorithms doesn't work with NaN values, 
+        therefore this method is built to treat them.
+        '''
+        # Drop all the instances with NaN
+        if type == 'dropna_row':
+            df.dropna(subset = cols, axis = 0)
+        # Drop all the feature that contains any NaN instance
+        elif type == 'drop_col':
+            df.dropna(subset = cols, axis = 1)
+        # Fill all the NaN values with its feature median value
+        elif type == 'fillna_median':
+            if cols == None: df.fillna(df.median())
+            else: df[cols] = df[cols].fillna(df.median())
+            return df, df.median()
+        # Fill all the NaN values with its feature mean value
+        elif type == 'fillna_mean':
+            if cols == None: df.fillna(df.median())
+            else: df[cols] = df[cols].fillna(df.mean())
+            return df, df.mean()
+        return df
+
+    def get_columns_of_specific_dtype(self, df, dtype):
+        return list(df.select_dtypes(include=[dtype]).columns)
